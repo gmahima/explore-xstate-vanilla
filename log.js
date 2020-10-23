264b7255dfcc405dfaf861ae251172478da68082
gmahima <mahimagangavarapu@gmail.com>
2020-10-24 00:12:19 +0530

    add log action

---
+++ log.js
@@ -0,0 +1,33 @@
+import Xstate from 'xstate'
+const {Machine, interpret, send, actions} = Xstate
+const {raise, respond, log} = actions
+
+const loggingMachine = Machine({
+    id: 'logging',
+    context: {
+        count: 42
+    },
+    initial: 'start',
+    states: {
+        start: {
+            entry: log('started'),
+            on: {
+                FINISH: {
+                    target: 'end',
+                    actions: log(
+                        (context, event) => `count: ${context.count}, event: ${event.type}`, 'Finish Label'
+                    )
+                }
+            }
+        },
+        end: {}
+    }
+})
+
+// const endState = loggingMachine.transition('start', 'FINISH')
+// console.log(endState.actions)
+const service = interpret(loggingMachine).start()
+service.send('FINISH')
+
+// output: started
+// Finish Label count: 42, event: FINISH
